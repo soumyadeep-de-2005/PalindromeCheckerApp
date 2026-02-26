fa526add0d8592359c0ccc94a115546b58462c3e
soumyadeep-de-2005 <de.soumyadeep5@gmail.com>
2026-02-26 15:26:20 +0530

    Added welcome message and startup screen

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,5 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome checker app management system");
+    }
+}git brancg
---
+++ App/src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Palindrome Checker App");
+        System.out.println("UC1: Application Entry & Welcome Message");
+        System.out.println("Version: 1.0");
+    }
+
+}
