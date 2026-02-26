d23f359f91df451a745bf47777895eb0201e888d
soumyadeep-de-2005 <de.soumyadeep5@gmail.com>
2026-02-26 15:40:01 +0530

    Reversing the string

--- App/src/UseCase3PalindromeCheckerApp.java
+++ App/src/UseCase3PalindromeCheckerApp.java
@@ -1,22 +1,21 @@
-public class UseCase2PalindromeCheckerApp {
+public class UseCase3PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
         String word = "madam";   // hardcoded string
         String reverse = "";
 
-        // reverse the string
+        // Reverse the string using loop
         for (int i = word.length() - 1; i >= 0; i--) {
             reverse = reverse + word.charAt(i);
         }
 
-        // check palindrome
+        // Compare original and reversed string
         if (word.equals(reverse)) {
             System.out.println("Palindrome Checker App");
-            System.out.println("UC2: Print a Hardcoded Palindrome Result");
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println("Palindrome Checker App");
-            System.out.println("UC2: Print a Hardcoded Palindrome Result");
             System.out.println(word + " is NOT a Palindrome");
         }
     }
